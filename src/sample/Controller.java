1c527e2f5eb8005633354a7cf9e341b9528ec1be
Dingram91 <Ddingram5146@eagle.fgcu.edu>
2019-09-16 19:16:14 -0400

    db push

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
+// make sure to do analys inspect code (no suspicios code found)
 
+// file - settings - pluggins - FindBugs-IDEA (Install) check code before submitting
+
+// Formatter ctrl - alt - L (use google-java-format) (want to see no lines changed, every file!
+//
+// one more file called check-style)
+
+// java-docs for every method note //* will generate some of the information automatically
+// view javadoc requirements in project page
+
+/**
+ * @author Dylan Ingram
+ */
 public class Controller {
 }
