71c08addd4af1cfe596318f6b3242670102288db
Dingram91 <Ddingram5146@eagle.fgcu.edu>
2019-09-25 19:18:48 -0400

    added css

--- src/productionApp/Main.java
+++ src/productionApp/Main.java
@@ -7,22 +7,27 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- *
+ * @author Dylan Ingram
  */
 public class Main extends Application {
 
+  /**
+   *
+   * @param primaryStage
+   * @throws Exception
+   */
   @Override
   public void start(Stage primaryStage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("mainScreen.fxml"));
     primaryStage.setTitle("Hello World");
-    primaryStage.setScene(new Scene(root, 800, 275));
+    root.getStylesheets().add
+        (Main.class.getResource("main.css").toExternalForm());
+    primaryStage.setScene(new Scene(root, 800, 500));
     primaryStage.show();
-
   }
 
 
   /**
-   *
    * @param args
    */
   public static void main(String[] args) {
